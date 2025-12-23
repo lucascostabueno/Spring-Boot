7f2888a2feb8210a63b206018e68b3ab28659ab9
lucascostabueno <lucasbueno722g@gmail.com>
2025-12-22 23:10:44 -0300

    secao 2 / aula 15: Obtendo os dados do produto

--- produtosapi/src/main/java/io/github/lucascostabueno/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/lucascostabueno/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package io.github.lucascostabueno.produtosapi.controller;
 
 import io.github.lucascostabueno.produtosapi.model.Produto;
 import io.github.lucascostabueno.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -27,5 +24,10 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
