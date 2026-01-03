f93c694412405da6c2df6a3a9c70bc933d06cb2b
lucascostabueno <lucasbueno722g@gmail.com>
2026-01-03 14:45:09 -0300

    secao 4 / aula 63: Entendendo sobre Lazy e Eager initializations

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/model/Livro.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/model/Livro.java
@@ -37,7 +37,7 @@ public class Livro {
     @Column(name = "preco", precision = 18, scale = 2)
     private BigDecimal preco;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_autor")
     private Autor autor;
 }
