5120f0dbe1a6adb6f503ec094fbbefe8108ec0a3
lucascostabueno <lucasbueno722g@gmail.com>
2026-01-04 21:50:14 -0300

    secao 4 / aula 66: Implementação de pesquisas com Query Methods

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
@@ -4,9 +4,16 @@ import io.github.cursodsousa.libraryapi.model.Autor;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface LivroRepository extends JpaRepository<Livro, Integer>  {
 
     List<Livro> FindByAutor(Autor autor);
+
+    List<Livro> findByTitulo(String titulo);
+
+    List<Livro> FindByIsbn(String isbn);
+
+    List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
 }
