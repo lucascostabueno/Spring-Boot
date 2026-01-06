5146e22d6e2b2423101c4186ac10b7593ec24e6e
lucascostabueno <lucasbueno722g@gmail.com>
2026-01-05 22:31:14 -0300

    secao 4 / aula 69: Utilizando Named e Positional Parameters

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/LivroRepository.java
@@ -1,8 +1,11 @@
 package io.github.cursodsousa.libraryapi.repository;
 
 import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.model.GeneroLivro;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -21,4 +24,29 @@ public interface LivroRepository extends JpaRepository<Livro, Integer>  {
     List<Livro> FindByTituloOrIsbn(String titulo, String isbn);
 
     List<Livro> FindByDataPublicacaoBetween (LocalDate inicio, LocalDate fim);
+
+    @Query("select l from Livro as l order by l.titulo, l.preco")
+    List<Livro> listarTodosOrdenadoPorTituloAndPreco();
+
+    //select a.
+    //from Livro l
+    //join autor a on a.id = l.id_autor
+    @Query("select a from Livro l join l.autor a")
+    List<Autor> listarAutoresDosLivros();
+
+    @Query(""" 
+    select l.genero
+    from Livro l
+    join l.autor a
+    where a.nacionalidade = 'Brasileira'
+    order by l.genero
+    """)
+    List<String> listarGenerosAutoresBrasileiros();
+
+    @Query("select l from Livro l where l.genero = :genero order by :paramOrdenacao")
+    List<Livro> findByGenero (
+            @Param("genero") GeneroLivro generolivro,
+            @Param("paramOrdenacao") String nomePropriedade
+    );
+
 }
