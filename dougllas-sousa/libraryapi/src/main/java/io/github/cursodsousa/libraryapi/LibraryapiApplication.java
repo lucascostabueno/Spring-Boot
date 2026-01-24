c5b567375067c2ae00ee5eedc0cdb6346a7555c4
lucascostabueno <lucasbueno722g@gmail.com>
2026-01-24 16:02:30 -0300

    secao 4 / aula 82: Adicionando Auditoria JPA nas entidades

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
@@ -1,9 +1,12 @@
 package io.github.cursodsousa.libraryapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
