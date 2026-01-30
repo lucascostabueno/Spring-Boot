46b34a8e8bf8bf08ff1bc1e7dd71102e107d7f21
lucascostabueno <lucasbueno722g@gmail.com>
2026-01-30 16:46:10 -0300

    secao 5 / aula 94: Explorando tipos de validação no Spring

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
@@ -3,16 +3,24 @@ package io.github.cursodsousa.libraryapi.controller.dto;
 import io.github.cursodsousa.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record AutorDTO(
         Integer id,
+
         @NotBlank(message = "campo obrigatorio")
+        @Size( min = 2, max = 100, message = "campo fora do tamanho padrao")
         String nome,
+
+        @Past(message = "nao pode ser data futura")
         @NotNull(message = "campo obrigatorio")
         LocalDate dataNascimento,
+
         @NotBlank(message = "campo obrigatorio")
+        @Size( min = 2, max = 100, message = "campo fora do tamanho padrao")
         String nacionalidade) {
 
     public Autor mapearParaAutor() {
