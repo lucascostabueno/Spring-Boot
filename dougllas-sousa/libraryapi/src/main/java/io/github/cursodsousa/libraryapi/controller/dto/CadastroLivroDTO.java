f7e5df20e509c71d128199631cd8a5f3371104b5
lucascostabueno <lucasbueno722g@gmail.com>
2026-03-23 21:45:18 -0300

    secao 7 / aula 98: Endpoint para cadastro de livro com Bean Validation

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/CadastroLivroDTO.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,15 +1,25 @@
 package io.github.cursodsousa.libraryapi.controller.dto;
 
 import io.github.cursodsousa.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatorio")
         String isbn,
+        @NotBlank(message = "campo obrigatorio")
         String titulo,
+        @NotNull(message = "campo obrigatorio")
+        @Past(message = "nao pode ser data futura")
         LocalDate dataPublicacao,
         GeneroLivro genero,
         BigDecimal preco,
+        @NotNull(message = "campo obrigatorio")
         Integer idAutor) {
 }
