d5f00cb9e0fa68dfb451d5bc40d56e2991b0d60e
lucascostabueno <lucasbueno722g@gmail.com>
2026-04-05 19:12:19 -0300

    secao 7 / aula 109: Criando joins ao utilizar Specifications JPA

--- dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/specs/LivroSpecs.java
+++ dougllas-sousa/libraryapi/src/main/java/io/github/cursodsousa/libraryapi/repository/specs/LivroSpecs.java
@@ -2,6 +2,8 @@ package io.github.cursodsousa.libraryapi.repository.specs;
 
 import io.github.cursodsousa.libraryapi.model.GeneroLivro;
 import io.github.cursodsousa.libraryapi.model.Livro;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class LivroSpecs {
@@ -16,5 +18,19 @@ public class LivroSpecs {
 
     public static Specification<Livro> generoEqual(GeneroLivro genero) {
         return (root, query, cb) -> cb.equal(root.get("genero"), genero);
+    }
+
+    public static Specification<Livro> anoPublicacaoEqual(Integer anoPublicacao) {
+        return (root, query, cb) ->
+                cb.equal(cb.function("to_char", String.class,
+                        root.get("dataPublicacao"), cb.literal("YYYY")), anoPublicacao.toString());
+    }
+
+    public static Specification<Livro> nomeAutorLike(String nome) {
+        return (root, query, cb) -> {
+            Join<Object, Object> joinAutor = root.join("autor", JoinType.INNER);
+            return cb.like(cb.upper(joinAutor.get("nome")), "%" + nome.toUpperCase() + "%");
+//            return cb.like(cb.upper(root.get("autor").get("nome")), "%" + nome.toUpperCase() + "%");
+        };
     }
 }
